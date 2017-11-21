59f33a03211bb481ab8e92cb7d29cb1934bd5e73
pethaf <peter@ctrl-c.liu.se>
2017-11-21 09:44:34 +0100

    Add timer

--- Euler2.py
+++ Euler2.py
@@ -1,9 +1,12 @@
+import time
 fibs = [1,2]
 maxFib = 4*10**6
 length = len(fibs)
+start_time = time.time();
 while(fibs[length-1]+fibs[length-2] < maxFib):
     fibs.append(fibs[length-1]+fibs[length-2])
     length = len(fibs)
 
 theSum = sum(filter(lambda arg: arg % 2 == 0,fibs))
+print(str(time.time()-start_time)+"\n")
 print(theSum)
