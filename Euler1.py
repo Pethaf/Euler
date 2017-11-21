76ba731f62e2c1508c87b57ab0828e5326fb964c
pethaf <peter@ctrl-c.liu.se>
2017-11-21 09:43:19 +0100

    Add timing

--- Euler1.py
+++ Euler1.py
@@ -1,5 +1,7 @@
+import time
+start_time = time.time()
 threes = 3*333*(333+1)/2
 fives = 5*199*(199+1)/2
 fifteens = 15*66*(66+1)/2
-
+print(str(time.time()-start_time) +"\n")
 print(threes+fives-fifteens)
